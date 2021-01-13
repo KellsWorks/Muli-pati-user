d3b183b6bf4e08daa901967a48802c0065d3fc3e
NEXTGEN CEO <56794404+KellsWorks@users.noreply.github.com>
2021-01-13 19:15:42 +0200

    Project start - phase 1
    Project start - phase 1

---
+++ app/src/main/java/app/mulipati/MainActivity.kt
@@ -0,0 +1,11 @@
+package app.mulipati
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
