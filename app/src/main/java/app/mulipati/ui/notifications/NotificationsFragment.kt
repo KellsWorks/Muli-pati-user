c70d9c3b330ed27a02b692d8f01517913caa78d0
NEXTGEN CEO <56794404+KellsWorks@users.noreply.github.com>
2021-01-14 22:47:02 +0200

    Notifications
    Notifications

--- app/src/main/java/app/mulipati/ui/notifications/NotificationsFragment.kt
+++ app/src/main/java/app/mulipati/ui/notifications/NotificationsFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import app.mulipati.R
 import app.mulipati.data.Notifications
 import app.mulipati.databinding.FragmentNotificationsBinding
@@ -51,5 +52,13 @@ class NotificationsFragment : Fragment() {
         notificationsBinding.viewedNotificationsRecycler
             .setController(controllerOne)
 
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        notificationsBinding.notificationsBack.setOnClickListener {
+            findNavController().navigateUp()
+        }
     }
 }
