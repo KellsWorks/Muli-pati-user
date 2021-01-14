bcb6d9b578f61ea5517791354f9690a136780ba3
NEXTGEN CEO <56794404+KellsWorks@users.noreply.github.com>
2021-01-14 07:33:43 +0200

    Onboarding
    On board

---
+++ app/src/main/java/app/mulipati/ui/OnBoarding.kt
@@ -0,0 +1,12 @@
+package app.mulipati.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import app.mulipati.R
+
+class OnBoarding : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_on_boarding)
+    }
+}
---
+++ app/src/main/java/app/mulipati/ui/splash/SplashScreen.kt
@@ -0,0 +1,12 @@
+package app.mulipati.ui.splash
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import app.mulipati.R
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+    }
+}
