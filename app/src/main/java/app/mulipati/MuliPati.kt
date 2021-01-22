eb7247ff4f6c63c8a82abc3ddef09e1eb2c57a7d
NEXTGEN CEO <56794404+KellsWorks@users.noreply.github.com>
2021-01-22 16:54:48 +0200

    JavaCompilationFeatures

--- app/src/main/java/app/mulipati/MuliPati.kt
+++ app/src/main/java/app/mulipati/MuliPati.kt
@@ -1,8 +1,10 @@
 package app.mulipati
 
 import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
 
+@HiltAndroidApp
 class MuliPati: Application() {
 
     override fun onCreate() {
